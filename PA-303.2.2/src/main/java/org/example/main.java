5c1c9d46868062444315f4b1f27e85c973600662
Jason Forrester <jason4ester1@gmail.com>
2024-07-09 11:51:09 -0500

    Java Stdin and stdout 1

---
+++ PA-303.2.2/src/main/java/org/example/main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.*;
+
+public class main {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        if(N >= 1 && N <= 100){
+            if(N % 2 != 0)
+            {
+                System.out.print("Weird");
+            }
+            else
+            {
+                if (N >= 2 && N <= 5){
+                    System.out.print("Not Weird");
+                }
+                else if (N >= 6 && N <= 20){
+                    System.out.print("Weird");
+                }
+                else{
+                    System.out.print("Not Weird");
+                }
+            }
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            scanner.close();
+        }
+    }
+}
